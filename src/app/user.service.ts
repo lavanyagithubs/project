d3cf8d67460412bd10fc8932a49f70820a352cf3
shreeshesh <shreeshesh007@gmail.com>
2021-03-29 14:40:34 +0530

    userservice added

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private hc:HttpClient) { }
+
+  
+  createUser(userObj):Observable<any>{
+    return this.hc.post("/user/register",userObj)
+  }
+
+ 
+
+  loginUser(userCredObj):Observable<any>{
+    return this.hc.post("/user/userDashboard",userCredObj)
+  }
+
+  changePassword(obj){
+    return this.hc.post("/user/passwordreset",obj)
+  }
 }
